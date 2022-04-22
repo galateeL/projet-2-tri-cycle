36084f87a2d154976f9fb7ce361d9e37836e4beb
Galatée Lardière <galatee.lardiere@gmail.com>
2022-04-22 11:23:23 +0200

    Create modal1 static answer

---
+++ frontend/src/components/ModalStaticAnswers.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function ModalStaticAnswers(props) {
+  return (
+    <div className="container">
+      {props.answer.correctAnswer ? (
+        <div className="good-answer">
+          <div className="img-good-answer">
+            <img src="assets/validate.png" alt="perfect" className="perfect" />
+          </div>
+        </div>
+      ) : (
+        <div className="wrong-answer">
+          <div className="img-wrong-answer">
+            <img src="assets/wrong.png" alt="wrong answer" />
+          </div>
+          <div className="correction">
+            <h5>la bonne réponse était ...</h5>
+            <img
+              src="assets/yellow-container.png"
+              alt="container jaune"
+              className="yellow-container-img"
+            />
+          </div>
+        </div>
+      )}
+      <div className="score">
+        <h4>mon score</h4>
+        <div className="myPoints">
+          <h4 className="points">{props.answer.score}</h4>
+        </div>
+      </div>
+
+      <div className="tip">
+        <img src="assets/idea.png" alt="tip" className="tip-img" />
+        <p className="tip-detail">{props.answer.tip}</p>
+      </div>
+
+      <div className="nextButton">
+        <button type="button" className="next">
+          Question suivante
+          <img src="assets/next-white.png" alt="prochaine question" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ModalStaticAnswers;
