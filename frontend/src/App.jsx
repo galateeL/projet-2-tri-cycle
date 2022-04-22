52c02da3e2feafcdf48ab7340921e59df76e9516
jago789654 <tony.remaud@hotmail.fr>
2022-04-22 10:55:27 +0200

    creation page quiz

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import Body from "@components/Body";
-import Footer from "@components/Footer";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Body />
-      <Footer />
     </div>
   );
 }
