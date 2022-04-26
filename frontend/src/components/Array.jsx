f1dd31744c686c935b7adcbc850b0784e1dcebef
jago789654 <tony.remaud@hotmail.fr>
2022-04-26 14:27:20 +0200

    Résolution mise à jour packages

--- frontend/src/components/Array.jsx
+++ frontend/src/components/Array.jsx
@@ -1,11 +1,19 @@
-import "@components/Array.css";
+import "@components/CssComponents/Array.css";
+import PropTypes from "prop-types";
 
-// function Array({ object }) {
-//   return (
-//     <div className="Array">
-//       <p>{object.description}</p>
-//     </div>
-//   );
-// }
+function Array({ object }) {
+  return (
+    <div className="Array">
+      <p>{object.description}</p>
+    </div>
+  );
+}
+Array.propTypes = {
+  // eslint-disable-next-line react/require-default-props
+  object: PropTypes.string,
+};
+// Array.defaultProps = {
+//   description: "no description",
+// };
 
-// export default Array;
+export default Array;
--- frontend/src/components/ButtonQuizz.jsx
+++ frontend/src/components/ButtonQuizz.jsx
@@ -1,19 +1,14 @@
 import { Link } from "react-router-dom";
-import styleButton from "../assets/quizzImg.png";
+import "./CssComponents/ButtonQuizz.css";
 
 export default function ButtonQuizz() {
-  let sectionStyle = {
-    backgroundImage: styleButton,
-  };
-
   return (
     <div>
       <Link to="/">
-        <button className="buttonImg" type="button">
-          <img src={sectionStyle.backgroundImage} className="imgButton" />
+        <button className="buttonQuiz" type="button">
+          bouton
         </button>
       </Link>
     </div>
   );
-  li;
 }
