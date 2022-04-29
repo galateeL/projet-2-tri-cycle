dda698e18f48be9f433a72f9bf6bdb959bf0bb03
rodrigue <rodrigue_techer@yahoo.com>
2022-04-29 13:04:48 +0200

    mise en place du tableau et des differents logos

--- frontend/src/pages/Research.jsx
+++ frontend/src/pages/Research.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Array from "@components/Array";
+import WasteTable from "@components/WasteTable";
+import Amount from "@components/Amount";
+import More from "@components/More";
 import axios from "axios";
-import FristArray from "@components/FirstArray";
+import "../components/CssComponents/WasteTable.css";
 import HeaderResearchBase from "@components/HeaderResearchBase";
 
 export default function Research() {
@@ -23,11 +25,37 @@ export default function Research() {
   return (
     <div className="Research">
       <HeaderResearchBase />
-      <div>
-        <FristArray key={waste.id} object={waste.fields} />
-        {waste.map((items) => (
-          <Array key={items.id} object={items.fields} />
-        ))}
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>amount</th>
+              <th>more</th>
+            </tr>
+          </thead>
+          <tbody>
+            {waste.map((items) => (
+              <tr>
+                <td>
+                  <WasteTable
+                    key={items.id}
+                    object={items.fields.description}
+                  />
+                </td>
+                <td>
+                  <Amount key={items.id} object={items.fields.reponse1} />
+                </td>
+                <td>
+                  <More
+                    key={items.id}
+                    object={items.fields.conseil_zero_dechet}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
