d1ea330afa59f9da605e51569040e30794243745
Talourd Clemence <clemence.talourd@laposte.net>
2022-05-02 00:11:49 +0200

    finitions CSS page home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,17 +15,14 @@ export default function Home() {
         </nav>
         <div className="logoTitre">
           <img className="logoImage" src={logo} alt="LOGO" />
-          <h1 className="triTitre">TriCycle</h1>
+          <h1 className="triTitre">Tri&apos;Cycle</h1>
         </div>
         <p className="presentation">
-          Sous le solum, chantum li zoisum di loremipsum ecolopsum.
-          Birdhitchcock campagnum etangum di fleurum verdum parcum pum.
-          Maitrumcorbum sur un arbrum perchum, lum tin api pre ce langagum. que
-          vum etum jolim que vou etum bo. Si votrum ramagum se rapelum a votrum
-          plumagum voum zetum etum le fenixum de se boim.loremipsum ecolopsum
-          birdhitchcock campagnum etangum di fleurum verdum parcum pum.
-          Maitrumcorbum sur un arbrum perchum, lum tin api pre ce langagum. que
-          vum etum jolim que vou etum bo.
+          Salut! Bienvenue sur Tri&apos;Cycle! Vous pourrez apprendre sur
+          l&apos;écologie, tout en vous amusant en famille! Cliquez sur le jeux
+          pour commencer votre Quizz favori, ou bien, cliquez sur recherche pour
+          accéder directement aux fiches informatives et devenir un expert du
+          tri! A vous de jouer!
         </p>
       </div>
       <div className="buttonFleche">
@@ -50,10 +47,11 @@ export default function Home() {
         </div>
 
         <p className="pQ">
-          loremipsum ecolopsum birdhitchcock campagnum etangum di fleurum verdum
-          parcum pum. Maitrumcorbum sur un arbrum perchum, lum tin api pre ce
-          langagum. que vum etum jolim que vou etum bo. Si votrum ramagum.n api
-          pre ce langagum. quevum etum jolim que vou etum bo.
+          Entrez dans notre quizz écologique, où de nombreuses questions sur le
+          tri des déchets vous attendent. Des fiches explicatives viendront
+          compléter votre curiosité et votre culture. Les questions sont
+          accessibles aux plus jeunes! Alors, êtes vous prêt à devenir
+          imbattable sur le tri?
         </p>
       </div>
       <div className="divSearch">
@@ -66,9 +64,10 @@ export default function Home() {
           />
         </div>
         <p className="pS">
-          loremipsum ecolopsum birdhitchcock campagnum etangum di fleurum verdum
-          parcum pum.loremipsum ecolopsum birdhitchcock campagnum etangum di
-          fleurum verdum parcum pum.n api pre ce langagum. que vum etum
+          Vous voulez jeter votre bidon de lessive, votre ampoule, votre clef à
+          molette, ou votre chaussette? Mais vous ne savez plus très bien où
+          vous en débarasser? Vous êtes au bon endroit! Ici vous trouverez votre
+          bonheur, et plus encore! Bienvenue dans la culture écologique du tri!
         </p>
       </div>
     </div>
