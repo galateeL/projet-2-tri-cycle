0191f2f183d17be06c47fbf3061c420f9879f0ed
jago789654 <tony.remaud@hotmail.fr>
2022-05-09 20:56:47 +0200

    mobileFirst

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,14 +9,10 @@ import "./PagesCss/Home.css";
 export default function Home() {
   return (
     <div>
-      <div>
-        <nav>
-          <Header />
-        </nav>
-        <div className="logoTitre">
-          <img className="logoImage" src={logo} alt="LOGO" />
-          <h1 className="triTitre">Tri&apos;cycle</h1>
-        </div>
+      <Header />
+      <div className="topPage">
+        <img className="logoImage" src={logo} alt="LOGO" />
+        <h1 className="triTitre">tri&apos;cycle</h1>
         <p className="presentation">
           Salut! Bienvenue sur Tri&apos;cycle! Vous pourrez apprendre sur
           l&apos;écologie, tout en vous amusant en famille! Cliquez sur le jeux
@@ -24,16 +20,14 @@ export default function Home() {
           accéder directement aux fiches informatives et devenir un expert du
           tri! A vous de jouer!
         </p>
-      </div>
-      <div className="buttonFleche">
         <ButtonFleche />
       </div>
-      <div className="triopoubelles">
+      <div className="tripoubelles">
         <h1>fais ton choix!</h1>
         <img
           className="logoTriPoubelles"
           src={trioPoubelles}
-          alt="trioPoubelles"
+          alt="triPoubelles"
         />
       </div>
       <div className="divQuizz">
@@ -47,8 +41,8 @@ export default function Home() {
         </div>
 
         <p className="pQ">
-          Entrez dans le quizz écologique! Prêt à devenir imbattable sur le ? ?
-          ? Go!
+          Entrez dans notre quizz écologique, où de nombreuses questions sur le
+          tri des déchets vous attendent. Prêt à devenir imbattable sur le tri ?
         </p>
       </div>
       <div className="divSearch">
@@ -61,8 +55,9 @@ export default function Home() {
           />
         </div>
         <p className="pS">
-          Jeter votre bidon, ou votre chaussette? Oui, mais où?... Ici vous
-          toutes nos informations sur le tri, et des astuces!
+          Vous voulez jeter votre bidon ou votre chaussette? Mais vous ne savez
+          plus très bien où vous en débarasser? Vous êtes au bon endroit!
+          Bienvenue dans la culture écologique du tri!
         </p>
       </div>
     </div>
