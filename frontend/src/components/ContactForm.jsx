e964ad43f209005b8b55947c04d019307542d6cf
Talourd Clemence <clemence.talourd@laposte.net>
2022-05-09 17:16:11 +0200

    modif contactForm

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import "./CssComponents/ContactForm.css";
+import emailimg1 from "../assets/email_1.png";
 
 export default function ContactForm() {
   const [name, setName] = useState("");
@@ -17,47 +18,56 @@ export default function ContactForm() {
           );
         }}
       >
-        <label className="labelform" htmlFor="name">
-          <h2 className="stringform">Nom :</h2>
-          <input
-            className="inputform"
-            id="name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label className="labelform" htmlFor="firstName">
-          <h2 className="stringform">Prénom :</h2>
-          <input
-            className="inputform"
-            id="firstName"
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label className="labelform" htmlFor="mail">
-          <h2 className="stringform">Email :</h2>
-          <input
-            className="inputform"
-            id="mail"
-            type="text"
-            value={mail}
-            onChange={(e) => setMail(e.target.value)}
-          />
-        </label>
-        <label className="labelaeraform" htmlFor="message">
-          <h2 className="stringform">Message:</h2>
-          <input
-            className="inputaeraform"
-            id="message"
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </label>
-        <input className="inputformsubmit" type="submit" value="Envoyer" />
+        <div className="contact">
+          <div className="emailone">
+            <img className="emailimg1" src={emailimg1} alt="emailimg1" />
+            <p>
+              Une suggestion, une remarque ou une question? N&apos;hésitez pas à
+              contacter l&apos;équipe <span>tri&apos;cycle</span> !
+            </p>
+          </div>
+          <label className="labelform" htmlFor="name">
+            <h2 className="stringform">Nom :</h2>
+            <input
+              className="inputform"
+              id="name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label className="labelform" htmlFor="firstName">
+            <h2 className="stringform">Prénom :</h2>
+            <input
+              className="inputform"
+              id="firstName"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </label>
+          <label className="labelform" htmlFor="mail">
+            <h2 className="stringform">Email :</h2>
+            <input
+              className="inputform"
+              id="mail"
+              type="text"
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
+            />
+          </label>
+          <label className="labelaeraform" htmlFor="message">
+            <h2 className="stringform">Message:</h2>
+            <input
+              className="inputaeraform"
+              id="message"
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </label>
+          <input className="inputformsubmit" type="submit" value="Envoyer" />
+        </div>
       </form>
     </div>
   );
